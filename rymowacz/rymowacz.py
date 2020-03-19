1eefd7899c795d6535310ba1299d4af09596a268
marekmaras83 <mike.krosno.team@gmail.com>
2020-03-19 12:03:01 +0100

    refactor

--- rymowacz/rymowacz.py
+++ rymowacz/rymowacz.py
@@ -1,59 +1,59 @@
 #! /usr/bin/python3
 
 VOVELS = ['a', 'ą', 'e', 'ę', 'i', 'o', 'ó', 'u', 'y']
-SOFTENING_PAIRS = {
+CENTRALIZED_DIPHTHONGS = {
     'i': VOVELS,
     'j': ['e', 'a'],
 }
 
 
-def is_multiple_vovel_syllable(word: str, i: int):
-    for softening_prefix, possible_suffixes in SOFTENING_PAIRS.items():
-        if word[i] == softening_prefix and word[i + 1] in possible_suffixes:
+def is_centralized_diphthong(word_to_check: str, letter_index: int):
+    for prefix, possible_suffixes in CENTRALIZED_DIPHTHONGS.items():
+        if word_to_check[letter_index] == prefix and word_to_check[letter_index + 1] in possible_suffixes:
             return True
     return False
 
 
 def has_one_syllable(word: str) -> bool:
-    first_syllable_found = False
+    first_harmony_found = False
     word_length = len(word)
 
     letter_index = 0
     while letter_index < word_length:
         if word[letter_index] in VOVELS:
-            if first_syllable_found:
+            if first_harmony_found:
                 return False
             else:
-                first_syllable_found = True
-                if letter_index < len(word)-1 and is_multiple_vovel_syllable(word, letter_index):
+                first_harmony_found = True
+                if letter_index < len(word)-1 and is_centralized_diphthong(word, letter_index):
                     letter_index += 1
         letter_index += 1
 
-    return first_syllable_found
+    return first_harmony_found
 
 
-def find_simple_rhyme_core(word: str) -> int:
+def find_single_tone_rhyme_harmony(word: str) -> int:
     reverse_enumerated_word = [(i, letter) for i, letter in enumerate(word)]
     reverse_enumerated_word.reverse()
 
     for letter_index, letter in reverse_enumerated_word:
         if letter in VOVELS:
-            if letter_index != 0 and is_multiple_vovel_syllable(word, letter_index-1):
+            if letter_index != 0 and is_centralized_diphthong(word, letter_index - 1):
                 return letter_index-1
             else:
                 return letter_index
 
 
-def get_rhyme_core(word: str) -> str:
-    first_core_index = find_simple_rhyme_core(word)
+def get_rhyme_harmony(word: str) -> str:
+    first_core_index = find_single_tone_rhyme_harmony(word)
     if has_one_syllable(word):
         return word[first_core_index::]
     else:
-        standard_rhyme_index = find_simple_rhyme_core(word[:first_core_index])
+        standard_rhyme_index = find_single_tone_rhyme_harmony(word[:first_core_index])
         return word[standard_rhyme_index::]
 
 
 if __name__ == "__main__":
     while True:
         word = input("Enter word to rhyme: ")
-        print(f"rhyme core for word {word} is {get_rhyme_core(word)}")
+        print(f"rhyme harmony for word '{word}' is '{get_rhyme_harmony(word)}'")
--- rymowacz/tests/test_rymowacz.py
+++ rymowacz/tests/test_rymowacz.py
@@ -1,6 +1,6 @@
 import pytest
 
-from rymowacz.rymowacz import has_one_syllable, get_rhyme_core, find_simple_rhyme_core
+from rymowacz.rymowacz import has_one_syllable, get_rhyme_harmony, find_single_tone_rhyme_harmony
 
 
 @pytest.mark.parametrize("input_word, expected_size_check, expected_simple_core, expected_core", [
@@ -11,12 +11,12 @@ from rymowacz.rymowacz import has_one_syllable, get_rhyme_core, find_simple_rhym
     ("boa", False, "a", "oa"), ("zoo", False, "o", "oo"),
     ("kozi", False, "i", "ozi"), ("mi", True, "i", "i"),
     ("dziewczynka", False, "a", "ynka"),
-    ("jojo", False, "o", "ojo"), ("jaja", False, "ja", "jaja"), ('zjem', True, "jem", "jem")
+    ("jojo", False, "o", "ojo"), ("jaja", False, "ja", "jaja"), ('zjem', True, "jem", "jem"),
 ])
-def test_get_rhyme_core(input_word, expected_size_check, expected_simple_core, expected_core):
+def test_get_rhyme_harmony(input_word, expected_size_check, expected_simple_core, expected_core):
     assert has_one_syllable(input_word) is expected_size_check
-    assert input_word[find_simple_rhyme_core(input_word)::] == expected_simple_core
-    assert get_rhyme_core(input_word) == expected_core
+    assert input_word[find_single_tone_rhyme_harmony(input_word)::] == expected_simple_core
+    assert get_rhyme_harmony(input_word) == expected_core
 
 
 # ====== niedokładne:
