09b3d66a80fb13bc5bb0e28a7d6b0b50bfefe13e
marekmaras83 <mike.krosno.team@gmail.com>
2020-03-19 22:42:40 +0100

    basic version of getting rhymes

--- rymowacz/tests/test_rymowacz.py
+++ rymowacz/tests/test_rymowacz.py
@@ -1,6 +1,8 @@
+from functools import reduce
+
 import pytest
 
-from rymowacz.rymowacz import get_rhyme_harmony, get_single_tone_rhyme_harmony
+from rymowacz.rymowacz import get_rhyme_harmony, get_single_tone_rhyme_harmony, get_rhymes
 
 
 @pytest.mark.parametrize("input_word, expected_single_harmony, expected_harmony", [
@@ -20,6 +22,32 @@ def test_get_rhyme_harmony(input_word, expected_single_harmony, expected_harmony
 
     assert get_rhyme_harmony(input_word) == expected_harmony
 
+
+@pytest.fixture
+def input_dictionary():
+    rhymes_per_harmony = {
+        "uba": ["buba", "podróba"],
+        "et": ["wnet", "net"],
+        "ebak": ["źrebak", "chlebak"],
+        "iem": ["wiem", "ciem"],
+        "ieczór": ["wieczór"],
+    }
+    return reduce(list.__add__, rhymes_per_harmony.values())
+
+
+@pytest.mark.parametrize("input_harmony, expected_rhymes", [
+    ("uba", ["buba"]),# "podróba"]),
+    ("et", ["wnet", "net"]), ("ebak", ['źrebak', "chlebak"]),
+    # ("wiem", ("iem", 1), "iem"), ('wieczór', ("ór", 5), "ieczór"),
+    # ("wij", ("ij", 1), "ij"), ("smutnie", ("ie", 5), "utnie"),
+    # ("to", ("o", 1), "o"),
+    # ("boa", ("a", 2), "oa"), ("zoo", ("o", 2), "oo"),
+    # ("kozi", ("i", 3), "ozi"), ("mi", ("i", 1), "i"),
+    # ("dziewczynka", ("a", 10), "ynka"),
+    # ("jojo", ("o", 3), "ojo"), ("jaja", ("ja", 2), "jaja"), ('zjem', ("jem", 1), "jem"),
+])
+def test_get_rhymes(input_harmony, expected_rhymes, input_dictionary):
+    assert get_rhymes(input_harmony, input_dictionary) == expected_rhymes
 
 # ====== niedokładne:
 # y/i
